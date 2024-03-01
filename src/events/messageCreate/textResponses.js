28de5e9f9b9b21e01fbc357d690c99c48cb37548
realfluffyuwu <realfluffyuwu@proton.me>
2024-03-01 17:49:40 +0800

    Just moved some things around

--- src/events/messageCreate/textResponses.js
+++ src/events/messageCreate/textResponses.js
@@ -5,12 +5,12 @@ var sovicFiles = fs.readdirSync("src/assets/sovicImages");
 var fileCount = sovicFiles.length
 
 module.exports = ( client, message ) => {
-    if (message.author.bot) return;
+	if (message.author.bot) return;
 
 	const text = message.content.toLowerCase();
 	const author = message.author;
 
-    if (text.includes("fluffy")) {
+	if (text.includes("fluffy")) {
 		switch (true) {
 			case (text.includes("fluffy is cool")):
 				message.reply(`Damn Straight! ${author} is spitting Straight Fire`)
@@ -38,14 +38,12 @@ module.exports = ( client, message ) => {
 	};
 
 	if (text.includes("sovic") || text.includes("hunter") || text.includes("habibi")) {
-	
-        var randomNumber = Math.floor(Math.random() * fileCount);
-        var randImage = sovicFiles[randomNumber];
+		var randomNumber = Math.floor(Math.random() * fileCount);
+		var randImage = sovicFiles[randomNumber];
 		message.reply({files: [`src/assets/sovicImages/${randImage}`]});
 	}
 
-    
 	if (text.includes("nigger") || text.includes("nigg")) {
-        message.reply(`https://media.discordapp.net/attachments/194772750957674497/1090295386473762916/attachment-2.gif?ex=65e73934&is=65d4c434&hm=72b0cd721f1e657f755a197dccd9623edf532c95c540c5347027e093a865d58c&`)
-    }
+		message.reply(`https://media.discordapp.net/attachments/194772750957674497/1090295386473762916/attachment-2.gif?ex=65e73934&is=65d4c434&hm=72b0cd721f1e657f755a197dccd9623edf532c95c540c5347027e093a865d58c&`)
+  }
 };
