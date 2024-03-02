15262875c7aa529ce7019157767132ed0f01218f
realfluffyuwu <realfluffyuwu@proton.me>
2024-03-02 22:41:17 +0800

    It's taken a lot of Bullshittery to get here

--- FluffyBotUwUDev.py
+++ FluffyBotUwUDev.py
@@ -1,24 +1,37 @@
+import pathlib
 import os
+import sys
 import discord
 from dotenv import load_dotenv
+from discord.ext import tasks, commands
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
+root_dir = pathlib.Path(__file__).parent
+command_dir = root_dir / 'commands'
+sys.path.append(str(command_dir))
+
 intents = discord.Intents.default()
 intents.message_content = True
 
+client = commands.Bot(command_prefix='/', intents=intents)
+#tree = discord.app_commands.CommandTree(client)
 
-client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}\n')
-    
-    print('Servers I am in:')
+    print(f'Startup Log: We have logged in as {client.user}')
+    print(f'Startup Log: Listing Servers that {client.user} is Apart of\n')
     for guild in client.guilds:
-        print(guild.name)
-    print(f'\nI am running in a total of {len(client.guilds)} Servers')
+        print(f'Name: {guild.name} ID: {guild.id}')
+    print(f'\nStartup Log: I am running in a total of {len(client.guilds)} Servers')
+    
+    print("Startup Log: Starting Presence Loop")
+    updatePresence.start()
+
+    #synced = await client.tree.sync()
+    print(f'Startup Log: Complete')
 
 @client.event
 async def on_message(message):
@@ -27,9 +40,45 @@ async def on_message(message):
         return
 
     # if the message starts with 'hello'
-    if message.content.startswith('hello'):
+    #if message.content.startswith('hello'):
         # we send a message back
-        await message.channel.send('Hello!')
+        #await message.channel.send('Hello!')
+
+presenceIndex = 0
+
+@tasks.loop(seconds = 10)
+async def updatePresence():
+    presenceMessages = [
+    	"Fuck Javascript",
+    	"Fluffy is Cool",
+    	"Broken Hand",
+    	"Windows Slave Name",
+    	"UwU",
+    	"Fibbin",
+    	"Arch user BTW",
+    	"Desk Puncher"
+    ]
+    
+    global presenceIndex
+    if presenceIndex > len(presenceMessages) - 1:
+        presenceIndex = 0
+    await client.change_presence(activity = discord.Game(presenceMessages[presenceIndex]))
+    presenceIndex += 1
+
+@tasks.loop(minutes = 1)
+async def automaticSync():
+    await client.tree.sync()
+
+# Easy way to find out if the Interaction Author is an admin
+# if action.permissions.administrator:
+#     print("User is Admin")
+
+
+@client.tree.command(name="hello", description="Replies with Hello!")
+async def hello(action: discord.Interaction):
+    if action.permissions.administrator:
+        print("User is Admin")
+    await action.response.send_message('Hello!')
 
 # Run the bot, The error can be disregarded, it's because python doesn't know TOKEN is a string
 client.run(TOKEN)
