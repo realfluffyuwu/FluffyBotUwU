e8538c687de8bfd6077702e27989b28815125da1
realfluffyuwu <realfluffyuwu@proton.me>
2024-03-02 18:30:11 +0800

    Main Bot File

---
+++ FluffyBotUwUDev.py
@@ -0,0 +1,32 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}\n')
+    
+    print('Servers I am in:')
+    for guild in client.guilds:
+        print(guild.name)
+    print(f'\nI am running in a total of {len(client.guilds)} Servers')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello fluffybot'):
+        await message.channel.send('Hello!')
+
+# Run the bot, The error can be disregarded, it's because python doesn't know TOKEN is a string
+client.run(TOKEN)
