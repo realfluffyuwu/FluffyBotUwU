c21809bda1a9781ed166e74e571ba77350d887c9
Jordon <realfluffyuwu@proton.me>
2024-08-06 09:39:16 +0800

    Added AFK slash command

--- commands/mitchtime.py
+++ commands/mitchtime.py
@@ -7,6 +7,37 @@ intents.message_content = True
 
 client = commands.Bot(command_prefix='/', intents=intents)
 
+
+
+
+
+verbs = [
+    "for a shave",
+    "for a walk",
+    "for a fly",
+    "to the vet",
+    "to the microwave",
+    "to a colonoscopy appointment",
+    "to get a triple bypass",
+    "to get a RGB Headset",
+        ]
+
+nouns = [
+        "Fish",
+        "Uncle",
+        "Crocodile",
+        "Sovic",
+        "Yowie",
+        "Tarantula",
+        "Rat",
+        "Goat",
+        "Cat",
+        "Dog",
+        "Turtle",
+        "Parrot",
+        "Dung Beetle"
+]
+
 def convertMitchTime(num: int = 0):
     measureMent = ["Minutes", "Hours", "Days", "Weeks", "Months", "Years"]
     timeScale = 0
@@ -43,7 +74,11 @@ def convertMitchTime(num: int = 0):
                         timeScale = 5
     return [round(mitchMinute), measureMent[timeScale]]
 
-class mitchminutes(commands.Cog):
+def getMitchTime():
+    rNum = r.randint(1, 400)
+    return convertMitchTime(rNum)
+
+class mitchtime(commands.Cog):
     def __init__(self, client):
         self.client = client
 
@@ -59,5 +94,17 @@ class mitchminutes(commands.Cog):
         await ctx.response.send_message("{author} is going afk for {num} Mitch {time}".format(author=author, num=mitchMinutes[0], time=mitchMinutes[1]))
         print("'{user}' executed '{command}' with parameter '{num}'".format(user=ctx.user, command=self.mitchminutes.qualified_name, num=minutes))
 
+    
+    @discord.app_commands.command(name="afk", description="Tells people how long and why you are going AFK")
+    async def afk(self, ctx: discord.Interaction):
+
+        author = ctx.user.display_name
+
+        mitchMinutes = getMitchTime()
+        message = "{user} has to go afk for {time} Mitch {measureMent} to take their pet {noun} {verb}".format(user=author, time=mitchMinutes[0], measureMent=mitchMinutes[1], noun=r.choice(nouns), verb=r.choice(verbs))
+
+        await ctx.response.send_message(message)
+        print("'{user}' executed '{command}'".format(user=ctx.user, command=self.afk.qualified_name))
+
 async def setup(client):
-    await client.add_cog(mitchminutes(client))
+    await client.add_cog(mitchtime(client))
